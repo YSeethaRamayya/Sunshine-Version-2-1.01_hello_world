ce9dc1bdb30b913cf76cee97fdfaba2fb00b73a4
Seetharamayya yeleswarapu <Seetharamayya yeleswarapu>
2016-08-05 11:01:10 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/android/sunshine/app/FetchWeatherTaskTest.java
@@ -0,0 +1,10 @@
+package com.example.android.sunshine.app;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ramayya on 8/3/2016.
+ */
+public class FetchWeatherTaskTest extends TestCase {
+
+}
